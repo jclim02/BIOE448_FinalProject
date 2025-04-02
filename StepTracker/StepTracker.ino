352375f223990d70f08b0fc3855bebe829b09a07
jclim02 <124019114+jclim02@users.noreply.github.com>
2025-04-02 14:34:38 -0500

    Add step count and modify threshold
    Testing got within 10% error

--- StepTracker/StepTracker.ino
+++ StepTracker/StepTracker.ino
@@ -9,12 +9,19 @@ BIOE 448 Final Project -- Step Counter
 */
 
 #include <Wire.h> // Necessary for I2C communication
-#
 
 int accel = 0x53; // I2C address for this sensor (from data sheet)
 float x, y, z;
-float total_accel;
+float x2, y2, z2;
+float x3, y3, z3;
+float total_accel_1, total_accel_2, total_accel_3;
+float accel_diff, accel_diff_2;
+float diff = 20000;
 float xavg, yavg, zavg;
+bool base_1, base_2;
+// float diff_1, diff_2;
+int step = 0;
+int timeout = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -43,17 +50,30 @@ void loop() {
   y = (Wire.read() | Wire.read() << 8); // Parse y values
   z = (Wire.read() | Wire.read() << 8); // Parse z values
 
-  // Serial.print("x = "); // Print values
-  // Serial.println(x);
-  // Serial.print(", y = ");
-  // Serial.print(y);
-  // Serial.print(", z = ");
-  // Serial.println(z);
+  total_accel_1 = sqrt(pow((x-xavg),2)+pow((y-yavg),2)+pow((z-zavg),2));
+  // Serial.println(total_accel_1);
 
-  total_accel = sqrt(pow((x-xavg),2)+pow((y-yavg),2)+pow((z-zavg),2));
-  // total_accel = sqrt(pow((x),2)+pow((y),2)+pow((z),2));
-  // total_accel = sqrt(x+y+z);
-  Serial.println(total_accel);
+  delay(50);
 
-  delay(200);
+  Wire.beginTransmission(accel);
+  Wire.write(0x32); // Prepare to get readings for sensor (address from data sheet)
+  Wire.endTransmission(false);
+  Wire.requestFrom(accel, 6, true); // Get readings (2 readings per direction x 3 directions = 6 values)
+  x2 = (Wire.read() | Wire.read() << 8); // Parse x values
+  y2 = (Wire.read() | Wire.read() << 8); // Parse y values
+  z2 = (Wire.read() | Wire.read() << 8); // Parse z values
+
+  total_accel_2 = sqrt(pow((x2-xavg),2)+pow((y2-yavg),2)+pow((z2-zavg),2));
+  // Serial.println(total_accel_2);
+
+  accel_diff = abs(total_accel_2 - total_accel_1);
+
+  if (accel_diff>diff){
+    step++;
+  }
+
+  Serial.println(step);
+
+  delay(100);
+
 }
