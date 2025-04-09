df4b8fe0f667f9c52a7b34c711de8f07e3d373eb
jclim02 <124019114+jclim02@users.noreply.github.com>
2025-04-09 14:34:13 -0500

    Add BLE functionality
    Remove IoT code

--- StepTracker/StepTracker.ino
+++ StepTracker/StepTracker.ino
@@ -9,8 +9,8 @@ BIOE 448 Final Project -- Step Counter
 */
 
 #include <Wire.h> // Necessary for I2C communication
-#include <LiquidCrystal.h>
-#include "thingProperties.h"
+#include <LiquidCrystal.h> // LCD
+#include <ArduinoBLE.h>
 
 // LCD
 LiquidCrystal lcd(10, 9, 5, 4, 3, 2);
@@ -21,8 +21,10 @@ float xavg, yavg, zavg; // reference values from boot
 float x, y, z;
 float x2, y2, z2;
 
-// IoT
-int counter = 0;
+// BLE
+BLEService newService("180A"); // creating the service
+BLEByteCharacteristic readChar("2A58", BLERead);
+BLEByteCharacteristic writeChar("2A57", BLEWrite);
 
 // Variables
 float total_accel_1, total_accel_2;
@@ -30,6 +32,7 @@ float accel_diff;
 float diff = 20000; // threshold difference
 int step = 0; // step count
 float distance = 0; // calculated distance
+long previousMillis = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -51,73 +54,91 @@ void setup() {
   zavg = (Wire.read() | Wire.read() << 8); // Parse z values
   Wire.endTransmission();
 
-  // IoT
-  delay(1500);
-  initProperties();
-  //Connect to cloud and get info/errors
-  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
-  setDebugMessageLevel(2);
-  ArduinoCloud.printDebugInfo();
-  //Wait for cloud connection
-  while (ArduinoCloud.connected() != 1) {
-      ArduinoCloud.update();
-      delay(500);
+  // BLE
+  while(!Serial);
+  if (!BLE.begin()){
+    Serial.println("Waiting for ArduinoBLE");
+    while(1);
   }
+
+  BLE.setLocalName("StepTracker");
+  BLE.setAdvertisedService(newService);
+  newService.addCharacteristic(readChar);
+  newService.addCharacteristic(writeChar);
+  BLE.addService(newService);
+
+  readChar.writeValue(0);
+  writeChar.writeValue(0);
+
+  BLE.advertise();
+  Serial.println("Bluetooth device active");
 }
 
 void loop() {
-  // obtain first set of values
-  Wire.beginTransmission(accel);
-  Wire.write(0x32); // Prepare to get readings for sensor (address from data sheet)
-  Wire.endTransmission(false);
-  Wire.requestFrom(accel, 6, true); // Get readings (2 readings per direction x 3 directions = 6 values)
-  x = (Wire.read() | Wire.read() << 8); // Parse x values
-  y = (Wire.read() | Wire.read() << 8); // Parse y values
-  z = (Wire.read() | Wire.read() << 8); // Parse z values
+  // BLE
+  BLEDevice central = BLE.central();
 
-  total_accel_1 = sqrt(pow((x-xavg),2)+pow((y-yavg),2)+pow((z-zavg),2));
-  // Serial.println(total_accel_1);
+  if (central){
+    Serial.print("Connected to central:");
+    Serial.println(central.address());
+    digitalWrite(LED_BUILTIN, HIGH);
 
-  delay(50);
+    while (central.connected()){
+      long currentMillis = millis();
+      
+      if (currentMillis - previousMillis >= 200) { 
+        previousMillis = currentMillis;
 
-  // obtain second set of values
-  Wire.beginTransmission(accel);
-  Wire.write(0x32); // Prepare to get readings for sensor (address from data sheet)
-  Wire.endTransmission(false);
-  Wire.requestFrom(accel, 6, true); // Get readings (2 readings per direction x 3 directions = 6 values)
-  x2 = (Wire.read() | Wire.read() << 8); // Parse x values
-  y2 = (Wire.read() | Wire.read() << 8); // Parse y values
-  z2 = (Wire.read() | Wire.read() << 8); // Parse z values
+        // Step and distance function
+        // obtain first set of values
+        Wire.beginTransmission(accel);
+        Wire.write(0x32); // Prepare to get readings for sensor (address from data sheet)
+        Wire.endTransmission(false);
+        Wire.requestFrom(accel, 6, true); // Get readings (2 readings per direction x 3 directions = 6 values)
+        x = (Wire.read() | Wire.read() << 8); // Parse x values
+        y = (Wire.read() | Wire.read() << 8); // Parse y values
+        z = (Wire.read() | Wire.read() << 8); // Parse z values
 
-  total_accel_2 = sqrt(pow((x2-xavg),2)+pow((y2-yavg),2)+pow((z2-zavg),2));
-  // Serial.println(total_accel_2);
+        total_accel_1 = sqrt(pow((x-xavg),2)+pow((y-yavg),2)+pow((z-zavg),2));
+        // Serial.println(total_accel_1);
 
-  accel_diff = abs(total_accel_2 - total_accel_1);
+        delay(50);
 
-  // if the difference in values is greater than the threshold, count a step
-  if (accel_diff > diff){
-    step++;
+        // obtain second set of values
+        Wire.beginTransmission(accel);
+        Wire.write(0x32); // Prepare to get readings for sensor (address from data sheet)
+        Wire.endTransmission(false);
+        Wire.requestFrom(accel, 6, true); // Get readings (2 readings per direction x 3 directions = 6 values)
+        x2 = (Wire.read() | Wire.read() << 8); // Parse x values
+        y2 = (Wire.read() | Wire.read() << 8); // Parse y values
+        z2 = (Wire.read() | Wire.read() << 8); // Parse z values
+
+        total_accel_2 = sqrt(pow((x2-xavg),2)+pow((y2-yavg),2)+pow((z2-zavg),2));
+        // Serial.println(total_accel_2);
+
+        accel_diff = abs(total_accel_2 - total_accel_1);
+
+        // if the difference in values is greater than the threshold, count a step
+        if (accel_diff > diff){
+          step++;
+        }
+
+        distance = step*5280/2000;
+
+        lcd.begin(16, 2); //Initiate the LCD in a 16x2 configuration
+        lcd.print("Steps: ");
+        lcd.print(step);
+        lcd.setCursor(0, 1);
+        lcd.print("Dist: ");
+        lcd.print(distance);
+        lcd.print("ft");
+        Serial.println(distance);
+
+        // BLE
+        readChar.writeValue(step);
+        
+        delay(100);
+      } 
+    }
   }
-
-  distance = step*5280/2000;
-
-  lcd.begin(16, 2); //Initiate the LCD in a 16x2 configuration
-  lcd.print("Steps: ");
-  lcd.print(step);
-  lcd.setCursor(0, 1);
-  lcd.print("Dist: ");
-  lcd.print(distance);
-  lcd.print("ft");
-  Serial.println(distance);
-
-// IoT
-  counter++;
-  if (counter > 50){
-    ArduinoCloud.update();
-    Serial.println(step);
-    counter = 0;
-  }
-
-  delay(100);
-
 }
